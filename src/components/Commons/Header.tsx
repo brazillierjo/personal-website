bdb2349561349566b04295aba982e4aa4c888884
brazillierjo <j.brazillier@gmail.com>
2022-12-11 19:53:45 +0100

    contact buttons

--- src/components/Commons/Header.tsx
+++ src/components/Commons/Header.tsx
@@ -1,25 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Header: React.FC<{}> = () => {
     return (
         <div className='flex h-12 items-center justify-between'>
-            <Link to='/'>
+            <NavLink to='/' style={({ isActive }) => (isActive ? { borderBottom: "2px solid red" } : {})}>
                 <h2 className='text-4xl font-extrabold text-primary'>
                     Johan<span className='text-secondary-100'>.</span>
                 </h2>
-            </Link>
+            </NavLink>
 
             <div className='flex h-full items-center gap-5 font-semibold'>
-                <Link
+                <NavLink
+                    style={({ isActive }) => (isActive ? { borderBottom: "2px solid red" } : {})}
                     className='border-secondary-100 transition-all duration-100 hover:border-b-2 hover:text-secondary-100'
                     to='/portfolio'>
                     Portfolio
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
+                    style={({ isActive }) => (isActive ? { borderBottom: "2px solid red" } : {})}
                     className='border-secondary-100 transition-all duration-100 hover:border-b-2 hover:text-secondary-100'
                     to='/contact'>
                     Contact
-                </Link>
+                </NavLink>
             </div>
         </div>
     );
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import ContactBanner from "../assets/png/contact.png";
+import Malt from "../assets/png/maltLogo.png";
+import Linkedin from "../assets/png/linkedin.png";
+import Mail from "../assets/png/mail.png";
 
 export const Contact: React.FC<{}> = () => {
     useEffect(() => {
@@ -18,28 +21,41 @@ export const Contact: React.FC<{}> = () => {
 
                 <p>Expérimenté et passionné, je serai heureux de vous aider à faire avancer votre projet.</p>
 
-                <div className='flex flex-wrap justify-evenly space-y-5 pt-12 lg:space-y-0'>
-                    <div className='flex flex-col space-y-5 rounded-lg bg-white p-6 text-center shadow-xl'>
-                        <p>Me contacter par mail</p>
-                        <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
-                            <a href='mailto:j.brazillier@gmail.com'>j.brazillier@gmail.com</a>
-                        </button>
+                <div className='flex flex-wrap justify-evenly gap-10 pt-12'>
+                    <div className='flex h-[130px] rounded-lg bg-white text-center shadow-xl lg:h-[230px]'>
+                        <div className='flex flex-col justify-center space-y-5 p-6'>
+                            <p>Me contacter par mail</p>
+                            <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
+                                <a href='mailto:j.brazillier@gmail.com' target='_blank'>
+                                    j.brazillier@gmail.com
+                                </a>
+                            </button>
+                        </div>
+                        <img className='rounded-lg' src={Mail} alt='Mail' />
                     </div>
-                    <div className='flex flex-col space-y-5 rounded-lg bg-white p-6 text-center shadow-xl'>
-                        <p>Me contacter via Linkedin</p>
-                        <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
-                            <a href='https://www.linkedin.com/in/johan-r-brazillier-9b917a174/' target='_blank'>
-                                LinkedIn
-                            </a>
-                        </button>
+
+                    <div className='flex h-[130px] rounded-lg bg-white text-center shadow-xl lg:h-[230px]'>
+                        <div className='flex flex-col justify-center space-y-5 p-6'>
+                            <p>Me contacter via LinkedIn</p>
+                            <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
+                                <a href='https://www.linkedin.com/in/johan-r-brazillier-9b917a174/' target='_blank'>
+                                    linkedin.fr
+                                </a>
+                            </button>
+                        </div>
+                        <img className='rounded-lg' src={Linkedin} alt='LinkedIn' />
                     </div>
-                    <div className='flex flex-col space-y-5 rounded-lg bg-white p-6 text-center shadow-xl'>
-                        <p>Me contacter via Malt</p>
-                        <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
-                            <a href='https://www.malt.fr/profile/brazillier' target='_blank'>
-                                malt.fr
-                            </a>
-                        </button>
+
+                    <div className='flex h-[130px] rounded-lg bg-white text-center shadow-xl lg:h-[230px]'>
+                        <div className='flex flex-col justify-center space-y-5 p-6'>
+                            <p>Me contacter via Malt</p>
+                            <button className='rounded-full bg-secondary-100 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-secondary-200'>
+                                <a href='https://www.malt.fr/profile/brazillier' target='_blank'>
+                                    malt.fr
+                                </a>
+                            </button>
+                        </div>
+                        <img className='rounded-lg' src={Malt} alt='Malt' />
                     </div>
                 </div>
             </div>
